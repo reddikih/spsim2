923aef298be493b0460e458fddf7c628bffa91e3
hikida satoshi <hikida@de.cs.titech.ac.jp>
2013-01-05 22:25:57 +0900

    implemented Request and Response class.

--- src/sim/Request.java
+++ src/sim/Request.java
@@ -2,4 +2,25 @@ package sim;
 
 public class Request {
 
+	private long key;
+	private int size;
+	private double arrvalTime;
+
+	public Request(long key, int size, double arrivalTime) {
+		this.key = key;
+		this.size = size;
+		this.arrvalTime = arrivalTime;
+	}
+
+	public long getKey() {
+		return key;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public double getArrvalTime() {
+		return arrvalTime;
+	}
 }
--- src/sim/Response.java
+++ src/sim/Response.java
@@ -2,4 +2,20 @@ package sim;
 
 public class Response {
 
+	private long key;
+	private double responseTime;
+
+	public Response(long key, double responseTime) {
+		this.key = key;
+		this.responseTime = responseTime;
+	}
+
+	public long getKey() {
+		return key;
+	}
+
+	public double getResponseTime() {
+		return responseTime;
+	}
+
 }
