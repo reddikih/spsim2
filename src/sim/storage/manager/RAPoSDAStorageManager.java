49c769c2104d54585140985ac474a0b7cd7daa41
hikida satoshi <hikida@de.cs.titech.ac.jp>
2013-01-05 14:32:14 +0900

    set remainder about StorageManager.

--- src/sim/storage/manager/RAPoSDAStorageManager.java
+++ src/sim/storage/manager/RAPoSDAStorageManager.java
@@ -2,4 +2,10 @@ package sim.storage.manager;
 
 public class RAPoSDAStorageManager {
 
+	/**
+	 * TODO
+	 * データディスクは，それぞれ担当するBlockIDの範囲を持っていること．
+	 * そうしないと，新しい書き込みは常にラウンドロビンでディスクに割り当てられ，キャッシュメモリのキューの増え方は，
+	 * 各メモリで常に同一となってしまう
+	 */
 }
