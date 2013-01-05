c83886bf53b6f24625adc24e39a5a8e1f486a863
hikida satoshi <hikida@de.cs.titech.ac.jp>
2013-01-05 09:21:04 +0900

    add RegionTest to AllTests test suite.

--- test/sim/AllTests.java
+++ test/sim/AllTests.java
@@ -5,11 +5,13 @@ import org.junit.runners.Suite;
 
 import sim.storage.DiskCacheTest;
 import sim.storage.HardDiskDriveTest;
+import sim.storage.manager.cmm.RegionTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	DiskCacheTest.class,
 	HardDiskDriveTest.class,
+	RegionTest.class,
 })
 
 public class AllTests {}
