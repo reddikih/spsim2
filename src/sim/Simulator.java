5556bd9b963f912a71204f36194848bf003b6c8a
Satoshi Hikida <s.hikida@hotmail.co.jp>
2013-01-24 17:38:39 +0900

    starting implementation of Simulator class.

--- src/sim/Simulator.java
+++ src/sim/Simulator.java
@@ -1,5 +1,25 @@
 package sim;
 
-public class Simulator {
+import sim.storage.manager.RAPoSDAStorageManager;
 
+public class Simulator {
+	
+	private void init() {
+		// TODO parse command line options
+		
+
+	}
+	
+	private double run() {
+		return 0.0;
+	}
+	
+	private void close() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+	
 }
