4b12b8a475123048c1705cc9f307253dc4243c0f
hikida satoshi <hikida@de.cs.titech.ac.jp>
2013-02-01 22:25:53 +0900

    add DataDiskTest case class into the test suite.

--- test/sim/AllTests.java
+++ test/sim/AllTests.java
@@ -9,6 +9,7 @@ import sim.storage.manager.cdm.CacheDiskTest;
 import sim.storage.manager.cdm.RAPoSDACacheDiskManagerTest;
 import sim.storage.manager.cmm.RAPoSDACacheMemoryManagerTest;
 import sim.storage.manager.cmm.RegionTest;
+import sim.storage.manager.ddm.DataDiskTest;
 import sim.storage.manager.ddm.RAPoSDADataDiskManagerTest;
 
 @RunWith(Suite.class)
@@ -18,6 +19,7 @@ import sim.storage.manager.ddm.RAPoSDADataDiskManagerTest;
 	RegionTest.class,
 	RAPoSDACacheMemoryManagerTest.class,
 	RAPoSDADataDiskManagerTest.class,
+	DataDiskTest.class,
 	CacheDiskTest.class,
 	RAPoSDACacheDiskManagerTest.class
 })
