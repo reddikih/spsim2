26c69d7179b6f4b0fc01dd8e29499d1e26423f07
hikida satoshi <hikida@de.cs.titech.ac.jp>
2013-06-22 16:42:11 +0900

    create EnergyEfficientBufferManager and its factory.
    for bfa_tik_06

---
+++ src/sim/storage/manager/buffer/EnergyEfficientBufferManagerFactory.java
@@ -0,0 +1,14 @@
+package sim.storage.manager.buffer;
+
+import sim.storage.manager.StorageManager;
+
+public class EnergyEfficientBufferManagerFactory implements
+		IBufferManagerFactory {
+
+	@Override
+	public IBufferManager createBufferManager(StorageManager sm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
