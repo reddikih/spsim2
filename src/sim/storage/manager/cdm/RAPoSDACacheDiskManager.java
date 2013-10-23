616a34deedfb0d35ed9cde3ce8aa2aa757d736cd
hikida satoshi <hikida@de.cs.titech.ac.jp>
2013-10-23 18:09:41 +0900

    avoid divid by zero.
    if the number of cache disks is zero, the read/write methods of RAPoSDACacheDiskManager return the defalut value. It's value is depend on the each method type.

--- src/sim/storage/manager/cdm/RAPoSDACacheDiskManager.java
+++ src/sim/storage/manager/cdm/RAPoSDACacheDiskManager.java
@@ -1,110 +1,114 @@
 package sim.storage.manager.cdm;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-
 import sim.Block;
 import sim.statistics.RAPoSDAStats;
 import sim.storage.CacheResponse;
 import sim.storage.DiskResponse;
 import sim.storage.util.RequestType;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
 public class RAPoSDACacheDiskManager implements ICacheDiskManager {
 
-	private int numberOfCacheDisks;
-	/**
-	 * key: cache disk id
-	 * value: cache disk object
-	 */
-	private HashMap<Integer, CacheDisk> cacheDisks;
+    private int numberOfCacheDisks;
+    /**
+     * key: cache disk id
+     * value: cache disk object
+     */
+    private HashMap<Integer, CacheDisk> cacheDisks;
 
-	/**
-	 * key: block id
-	 * value: cache disk id
-	 */
-	private HashMap<Long, Integer> block2cdMap;
+    /**
+     * key: block id
+     * value: cache disk id
+     */
+    private HashMap<Long, Integer> block2cdMap;
 
-	private int lastAssignedId;
+    private int lastAssignedId;
 
-	public RAPoSDACacheDiskManager(
-			int numberOfCacheDisks, HashMap<Integer, CacheDisk> cacheDiskMap) {
-		this.numberOfCacheDisks = numberOfCacheDisks;
-		this.cacheDisks = cacheDiskMap;
-		this.block2cdMap = new HashMap<Long, Integer>();
-	}
+    public RAPoSDACacheDiskManager(
+            int numberOfCacheDisks, HashMap<Integer, CacheDisk> cacheDiskMap) {
+        this.numberOfCacheDisks = numberOfCacheDisks;
+        this.cacheDisks = cacheDiskMap;
+        this.block2cdMap = new HashMap<Long, Integer>();
+    }
 
-	@Override
-	public DiskResponse write(Block[] blocks) {
-		if (blocks == null)
-			throw new IllegalArgumentException("blocks should not be null.");
+    @Override
+    public DiskResponse write(Block[] blocks) {
+        if (blocks == null)
+            throw new IllegalArgumentException("blocks should not be null.");
+        if (numberOfCacheDisks == 0)
+            return new DiskResponse(0.0, blocks);
 
-		CacheDisk cd;
-		CacheResponse cResp;
-		List<Block> writtenBlocks = new ArrayList<Block>();
-		double respTime = Double.MIN_VALUE;
+        CacheDisk cd;
+        CacheResponse cResp;
+        List<Block> writtenBlocks = new ArrayList<Block>();
+        double respTime = Double.MIN_VALUE;
 
-		for (Block b : blocks) {
-			int cdId;
-			if (block2cdMap.containsKey(b.getId())) {
-				// update write request
-				cdId = block2cdMap.get(b.getId());
-			} else {
-				// newly write
-				cdId = assignIdByRoundRobin();
-				block2cdMap.put(b.getId(), cdId);
-			}
-			cd = cacheDisks.get(cdId);
+        for (Block b : blocks) {
+            int cdId;
+            if (block2cdMap.containsKey(b.getId())) {
+                // update write request
+                cdId = block2cdMap.get(b.getId());
+            } else {
+                // newly write
+                cdId = assignIdByRoundRobin();
+                block2cdMap.put(b.getId(), cdId);
+            }
+            cd = cacheDisks.get(cdId);
 
-			cResp = cd.write(b);
+            cResp = cd.write(b);
 
-			// When a block replaced in the cache disk,
-			// then delete the corresponding entry of block2cdMap.
-			if (!cResp.getResult().equals(b))
-				deleteReplacedBlockFromMap(cResp.getResult());
+            // When a block replaced in the cache disk,
+            // then delete the corresponding entry of block2cdMap.
+            if (!cResp.getResult().equals(b))
+                deleteReplacedBlockFromMap(cResp.getResult());
 
-			respTime =
-				respTime < cResp.getResponseTime()
-				? cResp.getResponseTime() : respTime;
-			writtenBlocks.add(cResp.getResult());
-		}
-		return new DiskResponse(respTime, writtenBlocks.toArray(new Block[0]));
-	}
+            respTime =
+                    respTime < cResp.getResponseTime()
+                            ? cResp.getResponseTime() : respTime;
+            writtenBlocks.add(cResp.getResult());
+        }
+        return new DiskResponse(respTime, writtenBlocks.toArray(new Block[0]));
+    }
 
-	private void deleteReplacedBlockFromMap(Block block) {
-		block2cdMap.remove(block.getId());
-	}
+    private void deleteReplacedBlockFromMap(Block block) {
+        block2cdMap.remove(block.getId());
+    }
 
-	@Override
-	public CacheResponse read(Block block) {
-		if (block == null)
-			throw new IllegalArgumentException("block should not be null.");
+    @Override
+    public CacheResponse read(Block block) {
+        if (block == null)
+            throw new IllegalArgumentException("block should not be null.");
+        if (numberOfCacheDisks == 0)
+            return new CacheResponse(Double.MAX_VALUE, Block.NULL);
 
-		if (!block2cdMap.containsKey(block.getId())) {
-			// record statistics cache disk read
-			RAPoSDAStats.incrementCacheDiskAccessCount(RequestType.READ, false);
-			return new CacheResponse(Double.MAX_VALUE, Block.NULL);
-		}
+        if (!block2cdMap.containsKey(block.getId())) {
+            // record statistics cache disk read
+            RAPoSDAStats.incrementCacheDiskAccessCount(RequestType.READ, false);
+            return new CacheResponse(Double.MAX_VALUE, Block.NULL);
+        }
 
-		CacheDisk cd = cacheDisks.get(block2cdMap.get(block.getId()));
-		assert cd != null;
+        CacheDisk cd = cacheDisks.get(block2cdMap.get(block.getId()));
+        assert cd != null;
 
-		return cd.read(block);
-	}
+        return cd.read(block);
+    }
 
-	private int assignIdByRoundRobin() {
-		assert 0 <= lastAssignedId && lastAssignedId <= numberOfCacheDisks;
-		lastAssignedId = lastAssignedId % numberOfCacheDisks;
-		return lastAssignedId++;
-	}
+    private int assignIdByRoundRobin() {
+        assert 0 <= lastAssignedId && lastAssignedId <= numberOfCacheDisks;
+        lastAssignedId = lastAssignedId % numberOfCacheDisks;
+        return lastAssignedId++;
+    }
 
-	@Override
-	public void close(double closeTime) {
-		Collection<CacheDisk> cds = cacheDisks.values();
-		for (CacheDisk cd : cds) {
-			cd.close(closeTime);
-		}
-	}
+    @Override
+    public void close(double closeTime) {
+        Collection<CacheDisk> cds = cacheDisks.values();
+        for (CacheDisk cd : cds) {
+            cd.close(closeTime);
+        }
+    }
 
 }
--- test/sim/storage/cli/ClientTest.java
+++ test/sim/storage/cli/ClientTest.java
@@ -1,27 +1,27 @@
 package sim.storage.cli;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(JUnit4.class)
 public class ClientTest {
-	
-	@Test
-	public void registerInitialDataCheck() {
-		int blockSize = 1;
-		int numReplica = 1;
-		
-		String filePath = "test/sim/storage/cli/workload";
-		Client client = new Client(filePath);
-		
-		TestStorageManager sm =
-				new TestStorageManager(null,null,null,blockSize,numReplica);
-		
-		client.registerInitialData(sm);
-		assertThat(sm.requestMapSize(), is(10));
-	}
+
+    @Test
+    public void registerInitialDataCheck() {
+        int blockSize = 1;
+        int numReplica = 1;
+
+        String filePath = "test/sim/storage/cli/workload";
+        Client client = new Client(filePath);
+
+        TestStorageManager sm =
+                new TestStorageManager(null, null, null, blockSize, numReplica);
+
+        client.registerInitialData(sm);
+        assertThat(sm.requestMapSize(), is(10));
+    }
 }
