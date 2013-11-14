285b19a7f2fb906f4c6d13b70b90a93783694bfb
hikida satoshi <hikida@de.cs.titech.ac.jp>
2013-11-14 15:29:36 +0900

    fixed typo

--- src/sim/storage/manager/NormalStorageManager.java
+++ src/sim/storage/manager/NormalStorageManager.java
@@ -1,7 +1,5 @@
 package sim.storage.manager;
 
-import java.util.HashMap;
-
 import sim.Block;
 import sim.Request;
 import sim.Response;
@@ -11,75 +9,77 @@ import sim.storage.manager.cmm.NormalCacheMemoryManager;
 import sim.storage.manager.ddm.NormalDataDiskManager;
 import sim.storage.util.RequestType;
 
+import java.util.HashMap;
+
 public class NormalStorageManager extends StorageManager {
 
-	protected NormalCacheMemoryManager cmm;
-	protected NormalDataDiskManager ddm;
+    protected NormalCacheMemoryManager cmm;
+    protected NormalDataDiskManager ddm;
 
-	public NormalStorageManager(
-			NormalCacheMemoryManager cmm,
-			NormalDataDiskManager ddm,
-			int blockSize,
-			int numReplica) {
-		super(cmm, null, ddm);
+    public NormalStorageManager(
+            NormalCacheMemoryManager cmm,
+            NormalDataDiskManager ddm,
+            int blockSize,
+            int numReplica) {
+        super(cmm, null, ddm);
 
-		this.cmm = cmm;
-		this.ddm = ddm;
-		this.blockSize = blockSize;
-		this.numReplica = numReplica;
+        this.cmm = cmm;
+        this.ddm = ddm;
+        this.blockSize = blockSize;
+        this.numReplica = numReplica;
 
-		this.requestMap = new HashMap<Long, Block[]>();
-	}
+        this.requestMap = new HashMap<Long, Block[]>();
+    }
 
-	@Override
-	public Response read(Request request) {
-		Block[] blocks = requestMap.get(request.getKey());
-		if (blocks == null) throw new IllegalArgumentException();
+    @Override
+    public Response read(Request request) {
+        Block[] blocks = requestMap.get(request.getKey());
+        if (blocks == null) throw new IllegalArgumentException();
 
-		updateArrivalTimeOfBlocks(blocks, request.getArrvalTime());
+        updateArrivalTimeOfBlocks(blocks, request.getArrvalTime());
 
-		double respTime = Double.MIN_VALUE;
-		for (Block b : blocks) {
-			// TODO chenge to normal statistics.
-			// block access count log
-			RAPoSDAStats.incrementBlockAccessCount(RequestType.READ);
+        double respTime = Double.MIN_VALUE;
+        for (Block b : blocks) {
+            // TODO change to normal statistics.
+            // block access count log
+            RAPoSDAStats.incrementBlockAccessCount(RequestType.READ);
 
-			DiskResponse dResp = ddm.read(new Block[]{b});
-			respTime = respTime >= dResp.getResponseTime()
-			? respTime : dResp.getResponseTime();
-		}
-		return new Response(request.getKey(), respTime);
-	}
+            DiskResponse dResp = ddm.read(new Block[]{b});
+            respTime = respTime >= dResp.getResponseTime()
+                    ? respTime : dResp.getResponseTime();
+        }
+        return new Response(request.getKey(), respTime);
+    }
 
-	@Override
-	public Response write(Request request) {
-		Block[] blocks = requestMap.get(request.getKey());
-		if (blocks == null) {
-			// new request
-			blocks = divideRequest(request);
-			requestMap.put(request.getKey(), blocks);
-		} else {
-			// update(override) request
-			updateArrivalTimeOfBlocks(blocks, request.getArrvalTime());
-		}
+    @Override
+    public Response write(Request request) {
+        Block[] blocks = requestMap.get(request.getKey());
+        if (blocks == null) {
+            // new request
+            blocks = divideRequest(request);
+            requestMap.put(request.getKey(), blocks);
+        } else {
+            // update(override) request
+            updateArrivalTimeOfBlocks(blocks, request.getArrvalTime());
+        }
 
-		double respTime = Double.MIN_VALUE;
+        double respTime = Double.MIN_VALUE;
 
-		for (Block block : blocks) {
-			// block access count log
-			RAPoSDAStats.incrementBlockAccessCount(RequestType.WRITE);
+        for (Block block : blocks) {
+            // block access count log
+            RAPoSDAStats.incrementBlockAccessCount(RequestType.WRITE);
 
-			Block[] replicas = createReplicas(block);
+            Block[] replicas = createReplicas(block);
 
-			DiskResponse dResp = ddm.write(replicas);
-			respTime = respTime >= dResp.getResponseTime()
-			? respTime : dResp.getResponseTime();
-		}
-		return new Response(request.getKey(), respTime);
-	}
+            DiskResponse dResp = ddm.write(replicas);
+            respTime = respTime >= dResp.getResponseTime()
+                    ? respTime : dResp.getResponseTime();
+        }
+        return new Response(request.getKey(), respTime);
+    }
 
-	@Override
-	public void close(double closeTime) {
-		ddm.close(closeTime);
-	}
+    @Override
+    public void close(double closeTime) {
+        ddm.close(closeTime);
+    }
 }
--- src/sim/storage/manager/ddm/RAPoSDADataDiskManager.java
+++ src/sim/storage/manager/ddm/RAPoSDADataDiskManager.java
@@ -1,155 +1,154 @@
 package sim.storage.manager.ddm;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import sim.Block;
 import sim.storage.DiskResponse;
 import sim.storage.util.DiskInfo;
 import sim.storage.util.DiskState;
 import sim.storage.util.ReplicaLevel;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
 public class RAPoSDADataDiskManager implements IDataDiskManager {
 
-	private final int numberOfDataDisks;
-	private final int numberOfReplica;
+    private final int numberOfDataDisks;
+    private final int numberOfReplica;
 
-	private HashMap<Integer, DataDisk> dataDiskMap;
-	
-	private Logger spinupLogger = LoggerFactory.getLogger("SPIN_UP_INFO_TRACE");
+    private HashMap<Integer, DataDisk> dataDiskMap;
 
-	public RAPoSDADataDiskManager(
-			int numberOfDataDisks,
-			int numberOfReplica,
-			HashMap<Integer, DataDisk> dataDiskMap) {
+    private Logger spinupLogger = LoggerFactory.getLogger("SPIN_UP_INFO_TRACE");
 
-		if (numberOfDataDisks != dataDiskMap.size())
-			throw new IllegalArgumentException("number of data disks is invalid.");
+    public RAPoSDADataDiskManager(
+            int numberOfDataDisks,
+            int numberOfReplica,
+            HashMap<Integer, DataDisk> dataDiskMap) {
 
-		this.numberOfDataDisks = numberOfDataDisks;
-		this.numberOfReplica = numberOfReplica;
-		this.dataDiskMap = dataDiskMap;
-	}
+        if (numberOfDataDisks != dataDiskMap.size())
+            throw new IllegalArgumentException("number of data disks is invalid.");
 
-	@Override
-	public DiskResponse write(Block[] blocks) {
-		List<Block> writtenBlocks = new ArrayList<Block>();
-		double arrivalTime = blocks[0].getAccessTime();
-		double respTime = Double.MIN_VALUE;
-		for (Block block : blocks) {
-			int targetDiskId = block.getOwnerDiskId();
-			if (isSpinning(targetDiskId, arrivalTime)) {
-				DataDisk dd = dataDiskMap.get(targetDiskId);
-				assert dd != null;
-				double tempResp = dd.write(new Block[]{block});
-				respTime = respTime < tempResp ? tempResp : respTime;
-				writtenBlocks.add(block);
-			}
-		}
-		return new DiskResponse(respTime, writtenBlocks.toArray(new Block[0]));
-	}
+        this.numberOfDataDisks = numberOfDataDisks;
+        this.numberOfReplica = numberOfReplica;
+        this.dataDiskMap = dataDiskMap;
+    }
 
-	@Override
-	public DiskResponse read(Block[] blocks) {
-		double arrivalTime = blocks[0].getAccessTime();
-		double respTime = Double.MIN_VALUE;
-		for (Block block : blocks) {
-			int targetDiskId = block.getOwnerDiskId();
-			double latency = 0.0;
-			if (!isSpinning(targetDiskId, arrivalTime)) {
-				latency = spinUp(targetDiskId, arrivalTime);
-			}
-			block.setAccessTime(arrivalTime + latency);
+    @Override
+    public DiskResponse write(Block[] blocks) {
+        List<Block> writtenBlocks = new ArrayList<Block>();
+        double arrivalTime = blocks[0].getAccessTime();
+        double respTime = Double.MIN_VALUE;
+        for (Block block : blocks) {
+            int targetDiskId = block.getOwnerDiskId();
+            if (isSpinning(targetDiskId, arrivalTime)) {
+                DataDisk dd = dataDiskMap.get(targetDiskId);
+                assert dd != null;
+                double tempResp = dd.write(new Block[]{block});
+                respTime = respTime < tempResp ? tempResp : respTime;
+                writtenBlocks.add(block);
+            }
+        }
+        return new DiskResponse(respTime, writtenBlocks.toArray(new Block[0]));
+    }
 
-			DataDisk dd = dataDiskMap.get(targetDiskId);
-			assert dd != null;
-			// TODO this can be optimized for sequencial block access.
-			double ddResp = dd.read(new Block[]{block});
-			respTime = respTime < ddResp ? ddResp : respTime;
-		}
-		return new DiskResponse(respTime, blocks);
-	}
+    @Override
+    public DiskResponse read(Block[] blocks) {
+        double arrivalTime = blocks[0].getAccessTime();
+        double respTime = Double.MIN_VALUE;
+        for (Block block : blocks) {
+            int targetDiskId = block.getOwnerDiskId();
+            double latency = 0.0;
+            if (!isSpinning(targetDiskId, arrivalTime)) {
+                latency = spinUp(targetDiskId, arrivalTime);
+            }
+            block.setAccessTime(arrivalTime + latency);
 
-	public boolean isSpinning(int diskId, double accessTime) {
-		DataDisk dd = dataDiskMap.get(diskId);
-		assert dd != null;
-		return dd.isSpinning(accessTime);
-	}
+            DataDisk dd = dataDiskMap.get(targetDiskId);
+            assert dd != null;
+            // TODO this can be optimized for sequential block access.
+            double ddResp = dd.read(new Block[]{block});
+            respTime = respTime < ddResp ? ddResp : respTime;
+        }
+        return new DiskResponse(respTime, blocks);
+    }
 
-	public double spinUp(int diskId, double accessTime) {
-		DataDisk dd = dataDiskMap.get(diskId);
-		assert dd != null;
-		
-		// spin up log
-		spinupLogger.trace(
-				String.format("DISKID:%d ACCESSTIME:%,.4f", diskId, accessTime));
-		
-		return dd.spinUp(accessTime);
-	}
+    public boolean isSpinning(int diskId, double accessTime) {
+        DataDisk dd = dataDiskMap.get(diskId);
+        assert dd != null;
+        return dd.isSpinning(accessTime);
+    }
 
-	@Override
-	public int getNumberOfDataDisks() {
-		return numberOfDataDisks;
-	}
+    public double spinUp(int diskId, double accessTime) {
+        DataDisk dd = dataDiskMap.get(diskId);
+        assert dd != null;
 
-	@Override
-	public List<DiskInfo> getRelatedDisksInfo(Block block) {
-		List<DiskInfo> diskInfos = new ArrayList<DiskInfo>();
+        // spin up log
+        spinupLogger.trace(
+                String.format("DISKID:%d ACCESSTIME:%,.4f", diskId, accessTime));
 
-		int i = 0;
-		for (ReplicaLevel repLevel : ReplicaLevel.values()) {
-			if (numberOfReplica <= i) break;
-			int diskId = (block.getPrimaryDiskId() + i) % numberOfDataDisks;
-			DataDisk dd = dataDiskMap.get(diskId);
-			assert dd != null;
-			DiskInfo info = new DiskInfo(
-					diskId,
-					dd.getState(block.getAccessTime()),
-					repLevel);
-			diskInfos.add(info);
-			i++;
-		}
-		return diskInfos;
-	}
+        return dd.spinUp(accessTime);
+    }
 
-	public DiskInfo getLongestStandbyDiskInfo(
-			List<DiskInfo> diskInfos, double accessTime) {
+    @Override
+    public int getNumberOfDataDisks() {
+        return numberOfDataDisks;
+    }
 
-		DiskInfo longestSleeper = null;
-		double longestSleepTime = Double.MIN_VALUE;
-		for (DiskInfo dInfo : diskInfos) {
-			DataDisk dd = dataDiskMap.get(dInfo.getDiskId());
-			assert dd != null;
-			double sleepTime = dd.getStandbyTime(accessTime);
-			if (longestSleepTime < sleepTime) {
-				longestSleepTime = sleepTime;
-				longestSleeper = dInfo;
-			}
-		}
-		return longestSleeper;
-	}
+    @Override
+    public List<DiskInfo> getRelatedDisksInfo(Block block) {
+        List<DiskInfo> diskInfos = new ArrayList<DiskInfo>();
 
-	@Override
-	public void close(double closeTime) {
-		Collection<DataDisk> dds = dataDiskMap.values();
-		for (DataDisk dd : dds) {
-			dd.close(closeTime);
-		}
-	}
-	
-	public List<Integer> getSpecificStateDiskIds(double timestamp, List<DiskState> states) {
-		List<Integer> result = new ArrayList<Integer>();
-		for (DataDisk dd : this.dataDiskMap.values()) {
-			if (states.contains(dd.getState(timestamp))) {
-				result.add(dd.getId());
-			}
-		}
-		return result;
-	}
+        int i = 0;
+        for (ReplicaLevel repLevel : ReplicaLevel.values()) {
+            if (numberOfReplica <= i) break;
+            int diskId = (block.getPrimaryDiskId() + i) % numberOfDataDisks;
+            DataDisk dd = dataDiskMap.get(diskId);
+            assert dd != null;
+            DiskInfo info = new DiskInfo(
+                    diskId,
+                    dd.getState(block.getAccessTime()),
+                    repLevel);
+            diskInfos.add(info);
+            i++;
+        }
+        return diskInfos;
+    }
+
+    public DiskInfo getLongestStandbyDiskInfo(
+            List<DiskInfo> diskInfos, double accessTime) {
+
+        DiskInfo longestSleeper = null;
+        double longestSleepTime = Double.MIN_VALUE;
+        for (DiskInfo dInfo : diskInfos) {
+            DataDisk dd = dataDiskMap.get(dInfo.getDiskId());
+            assert dd != null;
+            double sleepTime = dd.getStandbyTime(accessTime);
+            if (longestSleepTime < sleepTime) {
+                longestSleepTime = sleepTime;
+                longestSleeper = dInfo;
+            }
+        }
+        return longestSleeper;
+    }
+
+    @Override
+    public void close(double closeTime) {
+        Collection<DataDisk> dds = dataDiskMap.values();
+        for (DataDisk dd : dds) {
+            dd.close(closeTime);
+        }
+    }
+
+    public List<Integer> getSpecificStateDiskIds(double timestamp, List<DiskState> states) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (DataDisk dd : this.dataDiskMap.values()) {
+            if (states.contains(dd.getState(timestamp))) {
+                result.add(dd.getId());
+            }
+        }
+        return result;
+    }
 
 }
