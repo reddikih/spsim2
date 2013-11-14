190eb3703e886d63cc465a457116c094e5083df6
hikida satoshi <hikida@de.cs.titech.ac.jp>
2013-11-14 15:31:32 +0900

    fixed something wrong

--- src/sim/storage/manager/NormalStorageManagerFactory.java
+++ src/sim/storage/manager/NormalStorageManagerFactory.java
@@ -1,7 +1,5 @@
 package sim.storage.manager;
 
-import java.util.HashMap;
-
 import sim.Parameter;
 import sim.storage.CacheParameter;
 import sim.storage.HDDParameter;
@@ -16,90 +14,92 @@ import sim.storage.manager.ddm.NormalDataDiskManager;
 import sim.storage.state.DiskStateParameter;
 import sim.storage.state.WithoutSleepDiskStateManager;
 
+import java.util.HashMap;
+
 public class NormalStorageManagerFactory extends StorageManagerFactory {
 
-	@Override
-	protected ICacheMemoryManager createCacheMemoryManager() {
-		CacheParameter param = new CacheParameter(
-				Parameter.CACHE_MEMORY_THRESHOLD,
-				Parameter.CACHE_MEMORY_SIZE,
-				Parameter.CACHE_MEMORY_LATENCY
-		);
+    @Override
+    protected ICacheMemoryManager createCacheMemoryManager() {
+        CacheParameter param = new CacheParameter(
+                Parameter.CACHE_MEMORY_THRESHOLD,
+                Parameter.CACHE_MEMORY_SIZE,
+                Parameter.CACHE_MEMORY_LATENCY
+        );
 
-		CacheMemory cm = null;
-		try {
-			CacheMemoryFactory factory =
-				(CacheMemoryFactory)Class.forName(
-						Parameter.CACHE_MEMORY_FACTORY).newInstance();
-			cm = factory.getCacheMemory(0, 1, param, Parameter.BLOCK_SIZE);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+        CacheMemory cm = null;
+        try {
+            CacheMemoryFactory factory =
+                    (CacheMemoryFactory) Class.forName(
+                            Parameter.CACHE_MEMORY_FACTORY).newInstance();
+            cm = factory.getCacheMemory(0, 1, param, Parameter.BLOCK_SIZE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
 //		return new NormalCacheMemoryManager(cm, Parameter.NUMBER_OF_REPLICA);
-		return new NormalCacheMemoryManager(cm);
-	}
+        return new NormalCacheMemoryManager(cm);
+    }
 
-	@Override
-	protected ICacheDiskManager createCacheDiskManager() {
-		// TODO consider either to create dummy ccm or not.
-		return null;
-	}
+    @Override
+    protected ICacheDiskManager createCacheDiskManager() {
+        // TODO consider either to create dummy ccm or not.
+        return null;
+    }
 
-	@Override
-	protected IDataDiskManager createDataDiskManager() {
-		int numdd =
-			Parameter.NUMBER_OF_CACHE_MEMORIES
-			* Parameter.NUMBER_OF_DISKS_PER_CACHE_GROUP;
-		int numRep = Parameter.NUMBER_OF_REPLICA;
-		HashMap<Integer, NormalDataDisk> dataDisks = new HashMap<Integer, NormalDataDisk>();
+    @Override
+    protected IDataDiskManager createDataDiskManager() {
+        int numdd =
+                Parameter.NUMBER_OF_CACHE_MEMORIES
+                        * Parameter.NUMBER_OF_DISKS_PER_CACHE_GROUP;
+        int numRep = Parameter.NUMBER_OF_REPLICA;
+        HashMap<Integer, NormalDataDisk> dataDisks = new HashMap<Integer, NormalDataDisk>();
 
-		HDDParameter param = new HDDParameter(
-				Parameter.HDD_SIZE,
-				Parameter.HDD_NUMBER_OF_PLATTER,
-				Parameter.HDD_RPM,
-				Parameter.HDD_CACHE_SIZE,
-				Parameter.HDD_TRANSFER_RATE,
-				Parameter.HDD_SECTORS_PER_TRACK,
-				Parameter.HDD_FULL_STROKE_SEEK_TIME,
-				Parameter.HDD_HEAD_SWITCH_OVERHEAD,
-				Parameter.HDD_COMMAND_OVERHEAD
-		);
+        HDDParameter param = new HDDParameter(
+                Parameter.HDD_SIZE,
+                Parameter.HDD_NUMBER_OF_PLATTER,
+                Parameter.HDD_RPM,
+                Parameter.HDD_CACHE_SIZE,
+                Parameter.HDD_TRANSFER_RATE,
+                Parameter.HDD_SECTORS_PER_TRACK,
+                Parameter.HDD_FULL_STROKE_SEEK_TIME,
+                Parameter.HDD_HEAD_SWITCH_OVERHEAD,
+                Parameter.HDD_COMMAND_OVERHEAD
+        );
 
-		WithoutSleepDiskStateManager ddstm =
-			new WithoutSleepDiskStateManager(
-					new DiskStateParameter(
-							Parameter.HDD_ACTIVE_POWER,
-							Parameter.HDD_IDLE_POWER,
-							Parameter.HDD_STANDBY_POWER,
-							Parameter.HDD_SPINDOWN_ENERGY,
-							Parameter.HDD_SPINDOWN_ENERGY,
-							Parameter.HDD_SPINDOWN_TIME,
-							Parameter.HDD_SPINUP_TIME
-					)
-			);
+        WithoutSleepDiskStateManager ddstm =
+                new WithoutSleepDiskStateManager(
+                        new DiskStateParameter(
+                                Parameter.HDD_ACTIVE_POWER,
+                                Parameter.HDD_IDLE_POWER,
+                                Parameter.HDD_STANDBY_POWER,
+                                Parameter.HDD_SPINDOWN_ENERGY,
+                                Parameter.HDD_SPINDOWN_ENERGY,
+                                Parameter.HDD_SPINDOWN_TIME,
+                                Parameter.HDD_SPINUP_TIME
+                        )
+                );
 
-		for (int i=0; i < numdd; i++) {
-			NormalDataDisk dd = new NormalDataDisk(i, param, ddstm);
-			dataDisks.put(i, dd);
-		}
-		return new NormalDataDiskManager(numdd, numRep, dataDisks);
-	}
+        for (int i = 0; i < numdd; i++) {
+            NormalDataDisk dd = new NormalDataDisk(i, param, ddstm);
+            dataDisks.put(i, dd);
+        }
+        return new NormalDataDiskManager(numdd, numRep, dataDisks);
+    }
 
-	@Override
-	protected StorageManager createStorageManager(ICacheMemoryManager cmm,
-			ICacheDiskManager cdm, IDataDiskManager ddm) {
+    @Override
+    protected StorageManager createStorageManager(ICacheMemoryManager cmm,
+                                                  ICacheDiskManager cdm, IDataDiskManager ddm) {
 
-		int blockSize = Parameter.BLOCK_SIZE;
-		int numRep = Parameter.NUMBER_OF_REPLICA;
+        int blockSize = Parameter.BLOCK_SIZE;
+        int numRep = Parameter.NUMBER_OF_REPLICA;
 
-		assert numRep > 0 : "number of replica parameter should greater than 0";
+        assert numRep > 0 : "number of replica parameter should greater than 0";
 
-		return new NormalStorageManager(
-				(NormalCacheMemoryManager)cmm,
-				(NormalDataDiskManager)ddm,
-				blockSize, numRep);
-	}
+        return new NormalStorageManager(
+                (NormalCacheMemoryManager) cmm,
+                (NormalDataDiskManager) ddm,
+                blockSize, numRep);
+    }
 
 }
